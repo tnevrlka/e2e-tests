6ddc393553aa568cefbe8d89c1e54c90be7b3495
Tomas Nevrlka <tnevrlka@redhat.com>
2023-04-03 16:33:13 +0200

    feat: added quay functions

---
+++ pkg/quay/api.go
@@ -0,0 +1,50 @@
+// Taken from https://github.com/redhat-appstudio/image-controller/blob/e7ced110d184bdb0935a9c39bbbf9ba3d9e8b359/pkg/quay/api.go
+
+package quay
+
+type Tag struct {
+	ImageId        string `json:"image_id"`
+	TrustEnabled   string `json:"trust_enabled"`
+	Name           string `json:"name"`
+	ManifestDigest string `json:"manifest_digest,omitempty"`
+	Size           int    `json:"int"`
+}
+
+type Repository struct {
+	TrustEnabled   bool           `json:"trust_enabled"`
+	Description    string         `json:"description"`
+	CanAdmin       bool           `json:"can_admin"`
+	CanWrite       bool           `json:"can_write"`
+	IsOrganization bool           `json:"is_organization"`
+	IsStarred      bool           `json:"is_starred"`
+	IsPublic       bool           `json:"is_public"`
+	Name           string         `json:"name"`
+	Namespace      string         `json:"namespace"`
+	Image          string         `json:"image"`
+	TagExpirationS int            `json:"tag_expiration_s"`
+	Tags           map[string]Tag `json:"tags"`
+	StatusToken    string         `json:"status_token"`
+	ErrorMessage   string         `json:"error_message"`
+	LastModified   string         `json:"last_modified"`
+}
+
+type RepositoryRequest struct {
+	Namespace   string `json:"namespace"`
+	Visibility  string `json:"visibility"`
+	Repository  string `json:"repository"`
+	Description string `json:"description"`
+	//Kind        string `json:"repo_kind"`
+}
+type RobotAccount struct {
+	Description string `json:"description"`
+	Created     string `json:"created"`
+	// UnstructuredData []byte  `json:"unstructured_metadata"`
+	LastAccessed string `json:"last_accessed"`
+	Token        string `json:"token"`
+	Name         string `json:"name"`
+	Message      string `json:"message"`
+}
+
+type QuayError struct {
+	ErrorMessage string `json:"error_type"`
+}
---
+++ pkg/quay/quay.go
@@ -0,0 +1,352 @@
+/*
+Copyright 2023 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Taken from https://github.com/redhat-appstudio/image-controller/blob/e7ced110d184bdb0935a9c39bbbf9ba3d9e8b359/pkg/quay/quay.go
+
+package quay
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/redhat-appstudio/e2e-tests/pkg/utils"
+)
+
+type QuayService interface {
+	CreateRepository(r RepositoryRequest) (*Repository, error)
+	DeleteRepository(organization, imageRepository string) (bool, error)
+	CreateRobotAccount(organization string, robotName string) (*RobotAccount, error)
+	DeleteRobotAccount(organization string, robotName string) (bool, error)
+	AddPermissionsToRobotAccount(organization, imageRepository, robotAccountName string) error
+}
+
+var _ QuayService = (*QuayClient)(nil)
+
+type QuayClient struct {
+	url        string
+	httpClient *http.Client
+	AuthToken  string
+}
+
+func NewQuayClient(c *http.Client, authToken, url string) QuayClient {
+	return QuayClient{
+		httpClient: c,
+		AuthToken:  authToken,
+		url:        url,
+	}
+}
+
+// CreateRepository creates a new Quay.io image repository.
+func (c *QuayClient) CreateRepository(r RepositoryRequest) (*Repository, error) {
+
+	url := fmt.Sprintf("%s/%s", c.url, "repository")
+
+	b, err := json.Marshal(r)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(b))
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", "Bearer", c.AuthToken))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	data := &Repository{}
+	err = json.Unmarshal(body, data)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	if res.StatusCode == 400 && data.ErrorMessage == "Repository already exists" {
+		data.Name = r.Repository
+	}
+	fmt.Println(string(body))
+	return data, nil
+}
+
+// DeleteRepository deletes specified image repository.
+func (c *QuayClient) DeleteRepository(organization, imageRepository string) (bool, error) {
+	url := fmt.Sprintf("%s/repository/%s/%s", c.url, organization, imageRepository)
+
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		return false, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", "Bearer", c.AuthToken))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == 204 {
+		return true, nil
+	}
+	if res.StatusCode == 404 {
+		return false, nil
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return false, err
+	}
+	data := &QuayError{}
+	err = json.Unmarshal(body, data)
+	if err != nil {
+		return false, err
+	}
+	return false, errors.New(data.ErrorMessage)
+}
+
+// CreateRobotAccount creates a new Quay.io robot account in the organization.
+func (c *QuayClient) CreateRobotAccount(organization string, robotName string) (*RobotAccount, error) {
+	url := fmt.Sprintf("%s/%s/%s/%s/%s", c.url, "organization", organization, "robots", robotName)
+
+	payload := strings.NewReader(`{
+  "description": "Robot account for AppStudio Component"
+}`)
+
+	req, err := http.NewRequest(http.MethodPut, url, payload)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", "Bearer", c.AuthToken))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	data := &RobotAccount{}
+	err = json.Unmarshal(body, data)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	if res.StatusCode == 400 && strings.Contains(data.Message, "Existing robot with name") {
+		req, err = http.NewRequest(http.MethodGet, url, &bytes.Buffer{})
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		req.Header.Add("Authorization", fmt.Sprintf("%s %s", "Bearer", c.AuthToken))
+		req.Header.Add("Content-Type", "application/json")
+
+		res, err := c.httpClient.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		data = &RobotAccount{}
+		err = json.Unmarshal(body, data)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+	}
+	fmt.Println(string(body))
+	return data, nil
+}
+
+// DeleteRobotAccount deletes given Quay.io robot account in the organization.
+func (c *QuayClient) DeleteRobotAccount(organization string, robotName string) (bool, error) {
+	url := fmt.Sprintf("%s/organization/%s/robots/%s", c.url, organization, robotName)
+
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		return false, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", "Bearer", c.AuthToken))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == 204 {
+		return true, nil
+	}
+	if res.StatusCode == 404 {
+		return false, nil
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return false, err
+	}
+	data := &QuayError{}
+	err = json.Unmarshal(body, data)
+	if err != nil {
+		return false, err
+	}
+	return false, errors.New(data.ErrorMessage)
+}
+
+// AddPermissionsToRobotAccount enables "robotAccountName" to "write" to "repository"
+func (c *QuayClient) AddPermissionsToRobotAccount(organization, imageRepository, robotAccountName string) error {
+
+	// example,
+	// url := "https://quay.io/api/v1/repository/redhat-appstudio/test-repo-using-api/permissions/user/redhat-appstudio+createdbysbose"
+
+	url := fmt.Sprintf("%s/repository/%s/%s/permissions/user/%s", c.url, organization, imageRepository, robotAccountName)
+	fmt.Println(url)
+	payload := strings.NewReader(`{
+	"role": "write"
+  }`)
+
+	req, err := http.NewRequest(http.MethodPut, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", "Bearer", c.AuthToken))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := c.httpClient.Do(req)
+	fmt.Println(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != 200 {
+		return fmt.Errorf("error adding permissions to the robot account, got status code %d", res.StatusCode)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println(string(body))
+	return nil
+}
+
+// Returns all repositories of the DEFAULT_QUAY_ORG organization
+func (c *QuayClient) GetAllRepositories() ([]Repository, error) {
+	url := fmt.Sprintf("%s/repository?last_modified=true&namespace=%s", c.url, utils.GetEnv("DEFAULT_QUAY_ORG", ""))
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", "Bearer", c.AuthToken))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		fmt.Printf("error getting repositories, got status code %d", res.StatusCode)
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	type Response struct {
+		Repositories []Repository
+	}
+	var response Response
+	json.Unmarshal(body, &response)
+	return response.Repositories, nil
+}
+
+// Returns all robot accounts of the DEFAULT_QUAY_ORG organization
+func (c *QuayClient) GetAllRobotAccounts() ([]RobotAccount, error) {
+	url := fmt.Sprintf("%s/organization/%s/robots", c.url, utils.GetEnv("DEFAULT_QUAY_ORG", ""))
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", "Bearer", c.AuthToken))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		fmt.Printf("error getting robot accounts, got status code %d", res.StatusCode)
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	type Response struct {
+		Robots []RobotAccount
+	}
+	var response Response
+	json.Unmarshal(body, &response)
+	return response.Robots, nil
+}
